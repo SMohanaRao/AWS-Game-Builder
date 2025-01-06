cafd1f582b952a2121016436beb29c77a66694fb
SMohanaRao <mohanarao.sanapala@gmail.com>
2025-01-06 20:48:40 +0530

    fix ts errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import config from '../amplify_outputs.json';  // Changed from ./amplify_outputs.json
 import '@aws-amplify/ui-react/styles.css';
 import App from './App';
 import './index.css';
+import config from '../amplify_outputs.json';
 
 // Configure Amplify with the new format
 Amplify.configure({
@@ -13,7 +13,7 @@ Amplify.configure({
     GraphQL: {
       endpoint: config.data.url,
       region: config.data.aws_region,
-      defaultAuthMode: config.data.default_authorization_type,
+      defaultAuthMode: 'userPool'
     }
   },
   Auth: {
@@ -27,9 +27,9 @@ Amplify.configure({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Authenticator>
-    {({ signOut, user }: { signOut?: () => void, user?: any }) => (
-      <App user={user} onSignOut={() => signOut?.()} />
-    )}
+      {({ signOut, user }: { signOut?: () => void, user?: any }) => (
+        <App user={user} onSignOut={() => signOut?.()} />
+      )}
     </Authenticator>
   </React.StrictMode>
 );
